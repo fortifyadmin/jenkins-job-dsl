d7a157831e377d0a77726c154a10f93a70d729d1
Ubuntu <ubuntu@ip-172-31-94-7.ec2.internal>
2021-01-29 04:45:17 +0000

    update repo

--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -2,7 +2,7 @@ job('NodeJS Docker example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('brandon@fortifyadmin.io')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('brandonspruth/docker-nodejs')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
